c56a374a7d6fd454cdc73a307a304dc0717461b3
sidharth tripathi <sidharthtripathi807@gmail.com>
2024-11-09 00:55:48 +0530

    fixed turborepo tasks

--- apps/webserver/build/lib/redis.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.redisDB = void 0;
-const redis_1 = require("redis");
-exports.redisDB = (0, redis_1.createClient)({
-    url: process.env.REDISDB_URL
-});
--- apps/webserver/build/schema/authSchema.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.loginSignupSchema = void 0;
-const zod_1 = require("zod");
-exports.loginSignupSchema = zod_1.z.object({
-    username: zod_1.z.string().min(1),
-    password: zod_1.z.string().min(8),
-});
